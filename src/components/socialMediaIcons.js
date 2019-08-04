cae4b5abdf0b6860b30d08c40aa15e7e76906527
SvenJungnickel <svenjungnickel@googlemail.com>
2019-08-04 18:03:23 +0200

    add rel and aria-label to social media buttons

--- src/components/socialMediaIcons.js
+++ src/components/socialMediaIcons.js
@@ -5,7 +5,14 @@ import Styles from '../styles/components/socialMediaIcons.module.scss';
 
 export default ({ items }) => {
     const SocialMediaIcon = ({ item }) => (
-        <Button href={item.link} size="lg" target="_blank" type="button">
+        <Button
+            href={item.link}
+            size="lg"
+            target="_blank"
+            type="button"
+            rel="noreferrer"
+            aria-label={`Visit Hayahay Cafe at ${item.type}`}
+        >
             {'facebook' === item.type && <FaFacebookF />}
             {'instagram' === item.type && <FaInstagram />}
         </Button>
