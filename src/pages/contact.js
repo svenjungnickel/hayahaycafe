5342f1e99b2cc0f4eb050a29275a026bbca7f2fb
SvenJungnickel <svenjungnickel@googlemail.com>
2019-08-07 00:20:24 +0200

    add google recaptcha to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,29 +1,67 @@
-import React, { useState } from 'react';
-import { graphql } from 'gatsby';
+import React, { useState, createRef } from 'react';
+import { graphql, navigate } from 'gatsby';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import SEO from '../components/seo';
+import Recaptcha from 'react-google-recaptcha';
 import Layout from '../components/layout';
+import SEO from '../components/seo';
+import Header from '../components/header';
 import ContactStyles from '../styles/pages/contact.module.scss';
 import SectionStyles from '../styles/components/section.module.scss';
-import Header from '../components/header';
+
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
+
+function encode(data) {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&');
+}
 
 export default ({ data }) => {
     const [validated, setValidated] = useState(false);
+    const [state, setState] = useState({});
+    const [recaptchaError, setRecaptchaError] = useState(false);
+    const recaptchaRef = createRef();
+
+    const handleChange = e => {
+        setState({ ...state, [e.target.name]: e.target.value });
+    };
+
+    const handleRecaptcha = () => {
+        setRecaptchaError(false);
+    };
 
     const handleSubmit = event => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
+        event.preventDefault();
+        event.stopPropagation();
+        setValidated(true);
+
+        const recaptchaValue = recaptchaRef.current.getValue() || false;
+        if (false === recaptchaValue) {
+            setRecaptchaError(true);
+            return;
         }
 
-        setValidated(true);
+        if (event.currentTarget.checkValidity() === false) {
+            return;
+        }
+
+        const form = event.target;
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({
+                'form-name': form.getAttribute('name'),
+                'g-recaptcha-response': recaptchaValue,
+                ...state,
+            }),
+        })
+            .then(() => navigate(form.getAttribute('action')))
+            .catch(error => alert(error));
     };
 
     return (
         <Layout currentPage="contact">
             <SEO title="Page two" keywords={[]} />
-
             <Header title="Contact" description="Feel free to contact us" header={data.header.childImageSharp.fluid} />
 
             <section className={SectionStyles.section}>
@@ -50,17 +88,20 @@ export default ({ data }) => {
                                 onSubmit={handleSubmit}
                                 name="contact"
                                 method="POST"
-                                action="/thanks?no-cache=1"
-                                // data-netlify-recaptcha="true"
+                                action="/thanks"
                                 data-netlify="true"
+                                data-netlify-recaptcha="true"
                             >
-                                <input type="hidden" name="form-name" value="contact" />
-
                                 <Form.Label>Name *</Form.Label>
                                 <Form.Row>
                                     <Col xs={12} lg={6}>
                                         <Form.Group controlId="contactFirstName">
-                                            <Form.Control name="firstName" type="text" required />
+                                            <Form.Control
+                                                name="firstName"
+                                                type="text"
+                                                required
+                                                onChange={handleChange}
+                                            />
                                             <Form.Text className="text-muted">First name</Form.Text>
                                             <Form.Control.Feedback type="invalid">
                                                 Please enter your first name.
@@ -69,7 +110,12 @@ export default ({ data }) => {
                                     </Col>
                                     <Col xs={12} lg={6}>
                                         <Form.Group controlId="contactLastName">
-                                            <Form.Control name="lastName" type="text" required />
+                                            <Form.Control
+                                                name="lastName"
+                                                type="text"
+                                                required
+                                                onChange={handleChange}
+                                            />
                                             <Form.Text className="text-muted">Last name</Form.Text>
                                             <Form.Control.Feedback type="invalid">
                                                 Please enter your last name.
@@ -80,7 +126,7 @@ export default ({ data }) => {
 
                                 <Form.Group controlId="contactEmail">
                                     <Form.Label>Email *</Form.Label>
-                                    <Form.Control name="email" type="email" required />
+                                    <Form.Control name="email" type="email" required onChange={handleChange} />
                                     <Form.Control.Feedback type="invalid">
                                         Please enter a valid email address.
                                     </Form.Control.Feedback>
@@ -88,7 +134,7 @@ export default ({ data }) => {
 
                                 <Form.Group controlId="contactSubject">
                                     <Form.Label>Subject *</Form.Label>
-                                    <Form.Control name="subject" type="text" required />
+                                    <Form.Control name="subject" type="text" required onChange={handleChange} />
                                     <Form.Control.Feedback type="invalid">
                                         Please enter a subject.
                                     </Form.Control.Feedback>
@@ -102,13 +148,27 @@ export default ({ data }) => {
                                         id="contactMessage"
                                         rows="5"
                                         required
+                                        onChange={handleChange}
                                     />
                                     <Form.Control.Feedback type="invalid">
                                         Please enter your message.
                                     </Form.Control.Feedback>
                                 </Form.Group>
 
-                                {/*<div data-netlify-recaptcha="true" />*/}
+                                <Form.Group controlId="contactRecaptcha">
+                                    <Recaptcha
+                                        id="contactRecaptcha"
+                                        ref={recaptchaRef}
+                                        sitekey={RECAPTCHA_KEY}
+                                        onChange={handleRecaptcha}
+                                    />
+                                    {true === recaptchaError && (
+                                        <div className="invalid-feedback" style={{ display: 'block' }}>
+                                            Please answer the captcha.
+                                        </div>
+                                    )}
+                                </Form.Group>
+
                                 <Button variant="primary" type="submit" className={ContactStyles.contactSubmitButton}>
                                     Submit
                                 </Button>
