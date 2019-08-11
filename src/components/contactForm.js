f2525c9416f11294192c6dc31770d6ef9f6400f9
SvenJungnickel <svenjungnickel@googlemail.com>
2019-08-11 21:46:07 +0200

    contact form success message animation

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,9 +1,12 @@
 import React, { useState, createRef } from 'react';
 import { Alert, Button, Col, Form } from 'react-bootstrap';
-import Styles from '../styles/pages/contactForm.module.scss';
 import Recaptcha from 'react-google-recaptcha';
+import Fade from 'react-reveal/Fade';
+import Styles from '../styles/pages/contactForm.module.scss';
 
 const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
+const CONTACT_FORM_FADE_UP_DURATION = 400;
+const SUCCESS_MESSAGE_FADE_IN_DURATION = 200;
 
 function encode(data) {
     return Object.keys(data)
@@ -58,94 +61,98 @@ export default () => {
             .catch(error => alert(error));
     };
 
-    const toggleSuccess = () => {
-        setSuccess(!success);
-    };
-
-    let successAlertStyle = [Styles.contactFormSuccess];
-    let contactFormStyle = [Styles.contactForm];
-    if (true === success) {
-        successAlertStyle.push(Styles.contactFormSuccessShow);
-        contactFormStyle.push(Styles.contactFormHide);
-    }
-
     return (
-        <>
-            <Form
-                noValidate
-                validated={validated}
-                onSubmit={handleSubmit}
-                name="contact"
-                method="POST"
-                action="/thanks"
-                data-netlify="true"
-                data-netlify-recaptcha="true"
-                className={contactFormStyle.join(' ')}
+        <div className={Styles.contactForm}>
+            <Fade top collapse when={false === success} duration={CONTACT_FORM_FADE_UP_DURATION} unmountOnExit={true}>
+                <Form
+                    noValidate
+                    validated={validated}
+                    onSubmit={handleSubmit}
+                    name="contact"
+                    method="POST"
+                    action="/thanks"
+                    data-netlify="true"
+                    data-netlify-recaptcha="true"
+                >
+                    <Form.Label>Name *</Form.Label>
+                    <Form.Row>
+                        <Col xs={12} lg={6}>
+                            <Form.Group controlId="contactFirstName">
+                                <Form.Control name="firstName" type="text" required onChange={handleChange} />
+                                <Form.Text className="text-muted">First name</Form.Text>
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter your first name.
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+                        <Col xs={12} lg={6}>
+                            <Form.Group controlId="contactLastName">
+                                <Form.Control name="lastName" type="text" required onChange={handleChange} />
+                                <Form.Text className="text-muted">Last name</Form.Text>
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter your last name.
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+
+                    <Form.Group controlId="contactEmail">
+                        <Form.Label>Email *</Form.Label>
+                        <Form.Control name="email" type="email" required onChange={handleChange} />
+                        <Form.Control.Feedback type="invalid">
+                            Please enter a valid email address.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group controlId="contactSubject">
+                        <Form.Label>Subject *</Form.Label>
+                        <Form.Control name="subject" type="text" required onChange={handleChange} />
+                        <Form.Control.Feedback type="invalid">Please enter a subject.</Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group controlId="contactMessage">
+                        <Form.Label>Message *</Form.Label>
+                        <textarea
+                            name="message"
+                            className="form-control"
+                            id="contactMessage"
+                            rows="5"
+                            required
+                            onChange={handleChange}
+                        />
+                        <Form.Control.Feedback type="invalid">Please enter your message.</Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group controlId="contactRecaptcha">
+                        <Recaptcha
+                            id="contactRecaptcha"
+                            ref={recaptchaRef}
+                            sitekey={RECAPTCHA_KEY}
+                            onChange={handleRecaptcha}
+                            className={Styles.contactFormReCaptcha}
+                        />
+                        {true === recaptchaError && (
+                            <div className="invalid-feedback" style={{ display: 'block' }}>
+                                Please answer the captcha.
+                            </div>
+                        )}
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" className={Styles.contactFormSubmitButton}>
+                        Submit
+                    </Button>
+                </Form>
+            </Fade>
+            <Fade
+                collapse
+                when={success}
+                duration={SUCCESS_MESSAGE_FADE_IN_DURATION}
+                delay={CONTACT_FORM_FADE_UP_DURATION}
             >
-                <Form.Label>Name *</Form.Label>
-                <Form.Row>
-                    <Col xs={12} lg={6}>
-                        <Form.Group controlId="contactFirstName">
-                            <Form.Control name="firstName" type="text" required onChange={handleChange} />
-                            <Form.Text className="text-muted">First name</Form.Text>
-                            <Form.Control.Feedback type="invalid">Please enter your first name.</Form.Control.Feedback>
-                        </Form.Group>
-                    </Col>
-                    <Col xs={12} lg={6}>
-                        <Form.Group controlId="contactLastName">
-                            <Form.Control name="lastName" type="text" required onChange={handleChange} />
-                            <Form.Text className="text-muted">Last name</Form.Text>
-                            <Form.Control.Feedback type="invalid">Please enter your last name.</Form.Control.Feedback>
-                        </Form.Group>
-                    </Col>
-                </Form.Row>
-
-                <Form.Group controlId="contactEmail">
-                    <Form.Label>Email *</Form.Label>
-                    <Form.Control name="email" type="email" required onChange={handleChange} />
-                    <Form.Control.Feedback type="invalid">Please enter a valid email address.</Form.Control.Feedback>
-                </Form.Group>
-
-                <Form.Group controlId="contactSubject">
-                    <Form.Label>Subject *</Form.Label>
-                    <Form.Control name="subject" type="text" required onChange={handleChange} />
-                    <Form.Control.Feedback type="invalid">Please enter a subject.</Form.Control.Feedback>
-                </Form.Group>
-
-                <Form.Group controlId="contactMessage">
-                    <Form.Label>Message *</Form.Label>
-                    <textarea
-                        name="message"
-                        className="form-control"
-                        id="contactMessage"
-                        rows="5"
-                        required
-                        onChange={handleChange}
-                    />
-                    <Form.Control.Feedback type="invalid">Please enter your message.</Form.Control.Feedback>
-                </Form.Group>
-
-                <Form.Group controlId="contactRecaptcha">
-                    <Recaptcha
-                        id="contactRecaptcha"
-                        ref={recaptchaRef}
-                        sitekey={RECAPTCHA_KEY}
-                        onChange={handleRecaptcha}
-                    />
-                    {true === recaptchaError && (
-                        <div className="invalid-feedback" style={{ display: 'block' }}>
-                            Please answer the captcha.
-                        </div>
-                    )}
-                </Form.Group>
-
-                <Button variant="primary" type="submit" className={Styles.contactFormSubmitButton}>
-                    Submit
-                </Button>
-            </Form>
-            <Alert variant="success" className={successAlertStyle.join(' ')}>
-                <p>{`Thank you for your message. We're getting in touch with you soon.`}</p>
-            </Alert>
-        </>
+                <Alert variant="success" className={Styles.contactFormSuccess}>
+                    <p>{`Thank you for your message. We're getting in touch with you soon.`}</p>
+                </Alert>
+            </Fade>
+        </div>
     );
 };
