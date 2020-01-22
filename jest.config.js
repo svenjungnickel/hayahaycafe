a492ab7905a96744f5a498fdc0286a55960e7d66
SvenJungnickel <svenjungnickel@googlemail.com>
2020-01-22 22:44:49 +0800

    add unit tests using jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    transform: {
+        '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
+    },
+    moduleNameMapper: {
+        '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/gatsby.js',
+    },
+    testPathIgnorePatterns: ['node_modules', '.cache', 'public'],
+    transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+    globals: {
+        __PATH_PREFIX__: '',
+    },
+    setupFiles: ['<rootDir>/loadershim.js'],
+    collectCoverage: true,
+    collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}', '!**/node_modules/**', '!**/vendor/**'],
+    coverageReporters: ['html', 'text-summary'],
+};
