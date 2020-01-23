d9ab6233a44b90243161c4b3ac5208b531ff8823
SvenJungnickel <svenjungnickel@googlemail.com>
2020-01-23 22:27:30 +0800

    moved tests and snapshots, update Header tests

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from '../Header';
+
+jest.mock('../Image');
+
+describe('Header', () => {
+    it('renders without subtitle', () => {
+        const headerImage = 'headerImage';
+        const title = 'title';
+
+        const component = <Header headerImage={headerImage} title={title} />;
+        const tree = renderer.create(component).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders with subtitle', () => {
+        const headerImage = 'headerImage';
+        const title = 'title';
+        const subtitle = 'subtitle';
+
+        const component = <Header headerImage={headerImage} title={title} subtitle={subtitle} />;
+        const tree = renderer.create(component).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
