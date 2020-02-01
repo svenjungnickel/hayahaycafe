110d73121aac3bb34b8ab202c46d3f6e8a91a4f1
SvenJungnickel <svenjungnickel@googlemail.com>
2020-02-02 00:57:21 +0800

    update text-muted color in dark mode for accessibility reasons, update snapshots

--- src/components/ContactForm/InputField.js
+++ src/components/ContactForm/InputField.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
+import ContactFormStyles from '../../styles/components/ContactForm/ContactForm.module.scss';
 
 const InputField = ({ controlId, name, label, errorMessage, onChange, description, ariaLabel }) => (
     <Form.Group controlId={controlId}>
         {'' !== label && <Form.Label>{label} *</Form.Label>}
         <Form.Control name={name} type="text" required onChange={onChange} aria-label={ariaLabel} />
-        {description && <Form.Text className="text-muted">{description}</Form.Text>}
+        {description && (
+            <Form.Text className={`text-muted ${ContactFormStyles.contactFormTextMuted}`}>{description}</Form.Text>
+        )}
         <Form.Control.Feedback type="invalid">{errorMessage}</Form.Control.Feedback>
     </Form.Group>
 );
