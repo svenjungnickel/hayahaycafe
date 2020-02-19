0104c99778bc13f8d0383fa308c97506c635285b
SvenJungnickel <svenjungnickel@googlemail.com>
2020-02-20 00:13:07 +0800

    Add Lighthouse audits via cypress
    - install and configure cypress-audit
    - update to cypress 4.0
    - create extra cypress config for lighthouse run
    - add Lighthouse audits for all pages
    - remove old lighthouserc.json and update gitignore
    - add new lighthouse github actions workflow
    - add command and description to README

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,17 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const { audit, prepareAudit } = require('cypress-audit');
+
 module.exports = (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
+
+    on('before:browser:launch', (browser = {}, launchOptions) => {
+        prepareAudit(launchOptions);
+    });
+
+    on('task', {
+        audit,
+    });
 };
