b07443fe6fa31ac90bc5be007da3a7b1ff5487ae
SvenJungnickel <svenjungnickel@googlemail.com>
2020-05-19 22:14:40 +0200

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
                 // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
                 // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
                 display: 'standalone',
-                icon: 'assets/icon.png', // This path is relative to the root of the site.
+                icon: 'assets/hayahay.png', // This path is relative to the root of the site.
             },
         },
         // This (optional) plugin enables Progressive Web App + Offline functionality.
