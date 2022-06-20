d2ba911b0285738ca50d5a550cc443d8cab3c75f
Sven Jungnickel <svenjungnickel@googlemail.com>
2022-06-20 23:41:05 +0200

    run lighthouse audits via lighthouse cli, add to ci workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('cypress');
-const { lighthouse, pa11y, prepareAudit } = require('cypress-audit');
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:8000/',
+        baseUrl: 'http://localhost:9000/',
         specPattern: 'cypress/e2e/*.test.js',
         video: false,
         videoUploadOnPasses: false,
@@ -14,16 +13,6 @@ module.exports = defineConfig({
         retries: {
             runMode: 2,
             openMode: 0,
-        },
-        setupNodeEvents(on, config) {
-            on('before:browser:launch', (browser = {}, launchOptions) => {
-                prepareAudit(launchOptions);
-            });
-
-            on('task', {
-                lighthouse,
-                pa11y,
-            });
         },
     },
 });
