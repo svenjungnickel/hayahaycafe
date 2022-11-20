a6040c00293278c6b3aa7275b55780f3c5a6afa2
Sven Jungnickel <svenjungnickel@googlemail.com>
2022-11-20 22:29:31 +0100

    refactor navbar e2e test suite

--- cypress/e2e/NavBar.test.js
+++ cypress/e2e/NavBar.test.js
@@ -20,109 +20,43 @@ const darkMode = () => {
     cy.get('[data-cy=navBar]').should('have.class', 'navbar-dark');
 };
 
-const homeLink = (mobile = false) => {
-    cy.visit('/404/');
+const checkLinks = (mobile, visit, selector, link, selector2 = null) => {
+    cy.visit(visit);
 
-    cy.get('[data-cy=navBarLinkHome]').should('have.attr', 'href').and('include', '/');
+    cy.get(selector).should('have.attr', 'href').and('include', link);
 
     if (true === mobile) {
         cy.get('[data-cy=navBarToggle]').click();
     }
 
-    cy.get('[data-cy=navBarLinkHome]').click();
+    cy.get(selector).click();
     cy.get('header');
-    cy.url().should('contain', '/');
-    cy.get('[data-cy=navBarLinkHome]').should('have.class', 'active');
+    cy.url().should('include', '/');
+    cy.get(selector2 ? selector2 : selector).should('have.class', 'active');
+};
+
+const homeLink = (mobile = false) => {
+    checkLinks(mobile, '/404/', '[data-cy=navBarLinkHome]', '/');
 };
 
 const logoLink = (mobile = false) => {
-    cy.visit('/404/');
-
-    cy.get('[data-cy=navBarLinkLogo]:visible').should('have.attr', 'href').and('include', '/');
-
-    if (true === mobile) {
-        cy.get('[data-cy=navBarToggle]').click();
-    }
-
-    cy.get('[data-cy=navBarLinkLogo]:visible').click();
-    cy.get('header');
-    cy.url().should('contain', '/');
-    cy.get('[data-cy=navBarLinkHome]').should('have.class', 'active');
+    checkLinks(mobile, '/404/', '[data-cy=navBarLinkLogo]:visible', '/', '[data-cy=navBarLinkHome]');
 };
 
 const menuLink = (mobile = false) => {
-    cy.visit('/');
-
-    cy.get('[data-cy=navBarLinkMenu]').should('have.attr', 'href').and('include', 'menu');
-
-    if (true === mobile) {
-        cy.get('[data-cy=navBarToggle]').click();
-    }
-
-    cy.get('[data-cy=navBarLinkMenu]').click();
-    cy.get('header');
-    cy.url().should('contain', '/menu/');
-    cy.get('[data-cy=navBarLinkMenu]').should('have.class', 'active');
-};
-
-const boodleBilaoLink = (mobile = false) => {
-    cy.visit('/');
-
-    cy.get('[data-cy=navBarLinkBoodleBilao]').should('have.attr', 'href').and('include', 'boodle-bilao');
-
-    if (true === mobile) {
-        cy.get('[data-cy=navBarToggle]').click();
-    }
-
-    cy.get('[data-cy=navBarLinkBoodleBilao]').click();
-    cy.get('header');
-    cy.url().should('contain', '/boodle-bilao');
-    cy.get('[data-cy=navBarLinkBoodleBilao]').should('have.class', 'active');
+    checkLinks(mobile, '/', '[data-cy=navBarLinkMenu]', 'menu');
 };
 
 const aboutLink = (mobile = false) => {
-    cy.visit('/');
-
-    cy.get('[data-cy=navBarLinkAbout]').should('have.attr', 'href').and('include', 'about');
-
-    if (true === mobile) {
-        cy.get('[data-cy=navBarToggle]').click();
-    }
-
-    cy.get('[data-cy=navBarLinkAbout]').click();
-    cy.get('header');
-    cy.url().should('contain', '/about');
-    cy.get('[data-cy=navBarLinkAbout]').should('have.class', 'active');
+    checkLinks(mobile, '/', '[data-cy=navBarLinkAbout]', 'about');
 };
 
 const galleryLink = (mobile = false) => {
-    cy.visit('/');
-
-    cy.get('[data-cy=navBarLinkGallery]').should('have.attr', 'href').and('include', 'gallery');
-
-    if (true === mobile) {
-        cy.get('[data-cy=navBarToggle]').click();
-    }
-
-    cy.get('[data-cy=navBarLinkGallery]').click();
-    cy.get('header');
-    cy.url().should('contain', '/gallery');
-    cy.get('[data-cy=navBarLinkGallery]').should('have.class', 'active');
+    checkLinks(mobile, '/', '[data-cy=navBarLinkGallery]', 'gallery');
 };
 
 const contactLink = (mobile = false) => {
-    cy.visit('/');
-
-    cy.get('[data-cy=navBarLinkContact]').should('have.attr', 'href').and('include', 'contact');
-
-    if (true === mobile) {
-        cy.get('[data-cy=navBarToggle]').click();
-    }
-
-    cy.get('[data-cy=navBarLinkContact]').click();
-    cy.get('header');
-    cy.url().should('contain', '/contact');
-    cy.get('[data-cy=navBarLinkContact]').should('have.class', 'active');
+    checkLinks(mobile, '/', '[data-cy=navBarLinkContact]', 'contact');
 };
 
 describe('Navigation bar', () => {
