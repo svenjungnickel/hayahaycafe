160615b654670524cba393e1b270f08d26035492
Sven Jungnickel <svenjungnickel@googlemail.com>
2023-01-30 16:15:14 +0800

    change gatsby serve port to avoid conflicts with docker port locally

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:9000/',
+        baseUrl: 'http://localhost:9090/',
         specPattern: 'cypress/e2e/*.test.js',
         video: false,
         videoUploadOnPasses: false,
